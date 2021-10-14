20c787121c947f238adeb88e5e3c770b18d3ff95
Monirul Islam Akand <45770524+monirulakand@users.noreply.github.com>
2021-10-14 16:48:53 +0600

    update2

---
+++ src/component/common/footer/Footer.js
@@ -0,0 +1,127 @@
+import React, {Component, Fragment} from 'react';
+import {Link} from "react-router-dom";
+
+class Footer extends Component {
+    render() {
+        return (
+            <Fragment>
+                <footer className="site-footer mt-5">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-12">
+                                <h1 className="FooterTitle mb-3">About</h1>
+                                <h6 className="FooterDes">Arena Web Security is a sister concern of Arena Web Technology. Arena Web Security is a renowned IT security company in Bangladesh since its establishment in 2012.</h6>
+                            </div>
+                            <div className="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-12">
+                                <h1 className="FooterTitle mb-3">Address</h1>
+                                <h6 className="FooterDes">House No-1, BLOCK-B Banasree, Main Road Rampura, Dhaka - 1219</h6>
+                                <h6 className="FooterDes">+880 1310&nbsp;333&nbsp;444, <br/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+880 1779&nbsp;224&nbsp;640</h6>
+                                <h6 className="FooterDes">info@arenawebsecurity.net</h6>
+                                <h6 className="FooterDes">www.arenawebsecurity.net</h6>
+                            </div>
+                            <div className="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-12">
+                                <h1 className="FooterTitle mb-3">Information</h1>
+                                <ul className="footer-links">
+                                    <li><Link className="FooterInfoLinkText" to="/About">About Us</Link></li>
+                                    <li><Link className="FooterInfoLinkText" to="/Contact">Contact</Link></li>
+                                    <li><Link className="FooterInfoLinkText" to="/Faq">FAQ</Link></li>
+                                </ul>
+                            </div>
+
+                            <div className="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-12">
+                                <h1 className="FooterTitle mb-3">Download</h1>
+                                <img className="Download-app mb-1 footer-links" src="../images/android.svg" alt=""/>
+                                <img className="Download-app mb-1 footer-links" src="../images/apple.svg" alt=""/>
+                            </div>
+                        </div>
+                        <hr/>
+
+                        <div className="row mt-5 mb-5">
+                            <div className="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-12">
+                                <h1 className="hotelText1 mb-3">Hotels Six Season</h1>
+                                <h1 className="hotelText1 mb-3">Hotels Sarina</h1>
+                                <h1 className="hotelText1 mb-3">Hotels Hilton City</h1>
+                                <h1 className="hotelText1 mb-3">Hotels pan pacific</h1>
+                                <h1 className="hotelText1 mb-3">Hotels The Mermaid</h1>
+                                <h1 className="hotelText1 mb-3">Hotels Sairu Hill</h1>
+                            </div>
+
+                            <div className="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-12">
+                                <h1 className="hotelText1 mb-3">Hotels Six Season</h1>
+                                <h1 className="hotelText1 mb-3">Hotels Sarina</h1>
+                                <h1 className="hotelText1 mb-3">Hotels Hilton City</h1>
+                                <h1 className="hotelText1 mb-3">Hotels pan pacific</h1>
+                                <h1 className="hotelText1 mb-3">Hotels The Mermaid</h1>
+                                <h1 className="hotelText1 mb-3">Hotels Sairu Hill</h1>
+                            </div>
+
+
+                            <div className="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-12">
+                                <h1 className="hotelText1 mb-3">Hotels Six Season</h1>
+                                <h1 className="hotelText1 mb-3">Hotels Sarina</h1>
+                                <h1 className="hotelText1 mb-3">Hotels Hilton City</h1>
+                                <h1 className="hotelText1 mb-3">Hotels pan pacific</h1>
+                                <h1 className="hotelText1 mb-3">Hotels The Mermaid</h1>
+                                <h1 className="hotelText1 mb-3">Hotels Sairu Hill</h1>
+                            </div>
+
+
+                            <div className="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-12">
+                                <h1 className="hotelText1 mb-3">Hotels Six Season</h1>
+                                <h1 className="hotelText1 mb-3">Hotels Sarina</h1>
+                                <h1 className="hotelText1 mb-3">Hotels Hilton City</h1>
+                                <h1 className="hotelText1 mb-3">Hotels pan pacific</h1>
+                                <h1 className="hotelText1 mb-3">Hotels The Mermaid</h1>
+                                <h1 className="hotelText1 mb-3">Hotels Sairu Hill</h1>
+                            </div>
+
+
+                            <div className="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-12">
+                                <h1 className="hotelText1 mb-3">Hotels Six Season</h1>
+                                <h1 className="hotelText1 mb-3">Hotels Sarina</h1>
+                                <h1 className="hotelText1 mb-3">Hotels Hilton City</h1>
+                                <h1 className="hotelText1 mb-3">Hotels pan pacific</h1>
+                                <h1 className="hotelText1 mb-3">Hotels The Mermaid</h1>
+                                <h1 className="hotelText1 mb-3">Hotels Sairu Hill</h1>
+                            </div>
+
+
+                            <div className="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-12">
+                                <h1 className="hotelText1 mb-3">Hotels Six Season</h1>
+                                <h1 className="hotelText1 mb-3">Hotels Sarina</h1>
+                                <h1 className="hotelText1 mb-3">Hotels Hilton City</h1>
+                                <h1 className="hotelText1 mb-3">Hotels pan pacific</h1>
+                                <h1 className="hotelText1 mb-3">Hotels The Mermaid</h1>
+                                <h1 className="hotelText1 mb-3">Hotels Sairu Hill</h1>
+                            </div>
+
+                        </div>
+
+                        <hr/>
+                    </div>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-8 col-sm-6 col-xs-12">
+                                <p className="copyright-text">Copyright &copy; 2021 All Rights Reserved by
+                                    <a className="FooterInfoLinkText" target="_blank" href="https://arenawebsecurity.net/"> Porzotok</a>.
+                                </p>
+                            </div>
+
+                            <div className="col-md-4 col-sm-6 col-xs-12">
+                                <ul className="social-icons">
+                                    <li><a className="facebook" target="_blank" href=""></a></li>
+                                    <li><a className="linkedin" target="_blank" href=""></a></li>
+                                    <li><a className="youTube" target="_blank" href=""></a></li>
+                                    <li><a className="facebook" target="_blank" href=""></a></li>
+                                    <li><a className="facebook" target="_blank" href=""></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </Fragment>
+        );
+    }
+}
+
+export default Footer;
---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import React, {Component, Fragment} from 'react';
+import Footer from "../../component/common/footer/Footer";
+
+class HomePage extends Component {
+    render() {
+        return (
+            <Fragment>
+             <Footer/>
+            </Fragment>
+        );
+    }
+}
+
+export default HomePage;
